92bb861c6a1838a51e8db3ed80983e5ea9a2ea37
Sitewku <Sitesentsii@gmail.com>
2026-02-20 09:16:28 +0200

    server deploy test fix

--- src/lib/CheckIfNextDay.ts
+++ src/lib/CheckIfNextDay.ts
@@ -2,6 +2,7 @@ export function checkIfNextDay(){
   const now = new Date();
   const hour = now.getHours();
   console.log(hour >= 15 ? 'Так це наступний день' : 'Ні це минулий')
+  console.log(hour)
   if(hour >= 15) return true
   else return false
 }
