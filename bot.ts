624fb6df81ad8b6c1eb0f9e47c6664bf84176198
Sitewku <Sitesentsii@gmail.com>
2026-02-20 22:43:08 +0200

    Code fixes and new features

--- bot.ts
+++ bot.ts
@@ -6,7 +6,7 @@ if (!TG_API_TOKEN) {
   throw new Error("BOT_TOKEN відсутній в .env");
 }
 
-import { mainMenu } from './src/view/menu.js';
+import { mainMenu, getSchedule } from './src/view/menu.js';
 
 import { editOrReplyMiddleware, type EditOrReplyFlavor } from "grammy-edit-or-reply";
 import { InputFile } from "grammy";
@@ -20,8 +20,12 @@ bot.use(editOrReplyMiddleware());
 bot.use(mainMenu)
 
 
+bot.command('schedule', async (ctx) => {
+  await getSchedule(ctx);
+})
+
 bot.command('start', async (ctx) => {
-  await ctx.reply("Вітаю! Це неофіційний бот для отримання розкладу занять СФК ЛНУП\nДодаткова інформація: /info", {
+  await ctx.reply("Вітаю! Це неофіційний бот для отримання розкладу занять СФК ЛНУП\nЗагляніть сюди за додатковою інформацією: /info", {
     reply_markup: mainMenu, 
   });
 })
@@ -29,7 +33,7 @@ bot.command('start', async (ctx) => {
 bot.command('info', async (ctx) => {
   await ctx.reply('<u>Інформація про проєкт</u>')
   await ctx.reply('Цей бот створений виключно в навчальних цілях. Автор не несе відповідальності за стабільну роботу, адже бот залежить від зовнішнього API tt.sclnau.com.ua та серверів, які він не контролює.\nЦе повністю волонтерський проєкт без зовнішнього фінансування, тому він може припинити існування, коли ресурси закінчаться.')
-  await ctx.reply('Автор: BattWkru (Гарасимів Іван)\nGitHub репозиторій: <a href="https://github.com/SitewrkU/sfk_tgbot">Github</a>\nОсобливості: /features\nІсторія розробки: /changelog\nОригінальний сайт: <a href="https://sclnau.com.ua/students/timetable-student.html">Сайт</a>\n❔Знайшли помилку або маєте ідею для вдосконалення? Пишіть сюди: @likebattw')
+  await ctx.reply('Автор: BattWkru (Гарасимів Іван)\nGitHub репозиторій: <a href="https://github.com/SitewrkU/sfk_tgbot">Github</a>\nОсобливості: /features\nІсторія розробки: /changelog\nОригінальний сайт: <a href="https://sclnau.com.ua/students/timetable-student.html">Сайт</a>\n❔Знайшли помилку або маєте ідею для вдосконалення? Пишіть сюди: @likebattw\n<tg-spoiler><a href="https://www.pixiv.net/en/tags/%E7%8C%AB%E5%AE%AE%E5%8F%88%E5%A5%88/illustrations">А це що за посилання?</a></tg-spoiler>')
 })
 
 bot.command('features', async (ctx) => {
--- src/view/menu.ts
+++ src/view/menu.ts
@@ -1,4 +1,5 @@
 import { Menu } from "@grammyjs/menu";
+import { Context } from "grammy";
 import { getCurrentDate, getNextDate } from "../lib/GetDate.js";
 import { checkIfNextDay } from "../lib/CheckIfNextDay.js";
 import { fixTime } from "../lib/FixPairTime.js";
@@ -6,56 +7,59 @@ import { getPairStatus } from "../lib/GetPairStatus.js";
 import { type EditOrReplyFlavor, editOrReplyMiddleware } from "grammy-edit-or-reply";
 import { DateTime } from "luxon";
 
+export async function getSchedule(ctx: Context) {
+  const isNextDay = checkIfNextDay();
+  const group = 'К-11';
+  
+  const loadingMsg = await ctx.reply(`🔃 Отримання даних...`);
+  const editLoading = (text: string) =>
+    ctx.api.editMessageText(ctx.chat!.id, loadingMsg.message_id, text);
+  
+  try {
+    let date = isNextDay ? getNextDate() : getCurrentDate();
+    let data = await fetch(`https://tt.sclnau.com.ua/student/GetStudent.php?group=${group}&date=${date}`)
+      .then(r => r.json());
+    
+    if ((!data.schedule || data.schedule.length === 0) && isNextDay) {
+      await editLoading('⚠️ Розклад на завтра відсутній. Спроба повернути сьогоднішній...');
+      date = getCurrentDate();
+      data = await fetch(`https://tt.sclnau.com.ua/student/GetStudent.php?group=${group}&date=${date}`)
+        .then(r => r.json());
+    }
+    
+    if (!data.schedule || data.schedule.length === 0) {
+      await editLoading('❌ Розклад відсутній на обидва дні.');
+      return;
+    }
+    
+    const schedule = data.schedule.map((item: { name: any; }) => item);
+    await editLoading(`✅ Розклад на <u><b>${date}</b></u> (${date === getCurrentDate() ? 'Сьогодні' : 'Завтра'}).\nГрупа: ${group}`);
+    
+    const pairStatus = getPairStatus(schedule);
+    let scheduleResult: string = '';
+    
+    for (const item of schedule) {
+      const time = fixTime(item.time, item.pairNumber);
+      const isCurrentPair = pairStatus?.pairNum === item.pairNumber;
+      const pairStatusText = isCurrentPair ? pairStatus!.text : '';
+      scheduleResult += `[${item.pairNumber}] <b>${item.subject}</b> ${pairStatusText ? ' | ' + pairStatusText : ''}\nАудиторія: <b>${item.room}</b> | 🕑 <b>${time}</b>` + "\n\n";
+    }
+    
+    await ctx.reply(scheduleResult, { parse_mode: 'HTML' });
+  } catch (err) {
+    await ctx.api.editMessageText(
+      ctx.chat!.id,
+      loadingMsg.message_id,
+      '❌ Не вдалося отримати дані'
+    );
+    console.log(err);
+  }
+}
+
+
 export const mainMenu = new Menu('main-menu')
   .text("Розклад", async (ctx) => {
-    const isNextDay = checkIfNextDay()
-    const group = 'К-11'
-    
-    const loadingMsg = await ctx.reply(`🔃 Отримання даних...`);
-    const editLoading = (text: string) =>
-      ctx.api.editMessageText(ctx.chat!.id, loadingMsg.message_id, text);
-
-    try {
-      let date = isNextDay ? getNextDate() : getCurrentDate();
-      let data = await fetch(`https://tt.sclnau.com.ua/student/GetStudent.php?group=${group}&date=${date}`)
-        .then(r => r.json());
-
-      if((!data.schedule || data.schedule.length === 0) && isNextDay){
-        await editLoading('⚠️ Розклад на завтра відсутній. Спроба поверути сьогоднішній...')
-        date = getCurrentDate();
-        data = await fetch(`https://tt.sclnau.com.ua/student/GetStudent.php?group=${group}&date=${date}`)
-          .then(r => r.json());
-      }
-
-      if (!data.schedule || data.schedule.length === 0) {
-        await editLoading('❌ Розклад відсутній на обидва дні.');
-        return;
-      }
-
-      const schedule = data.schedule.map((item: { name: any; }) => item); 
-      await editLoading(`✅ Розклад на <u><b>${date}</b></u> (${date === getCurrentDate() ? 'Сьогодні' : 'Завтра'}).\nГрупа: ${group}`)
-
-      // const testTime = DateTime.fromObject({ hour: 10, minute: 5 }, { zone: 'Europe/Kyiv' }); Для тестування
-      const pairStatus = getPairStatus(schedule)
-      let scheduleResult: string = '';
-      for(const item of schedule){
-        const time = fixTime(item.time, item.pairNumber)
-        const isCurrentPair = pairStatus?.pairNum === item.pairNumber;
-        const pairStatusText = isCurrentPair ? pairStatus!.text : '';
-        scheduleResult += `[${item.pairNumber}] <b>${item.subject}</b> ${pairStatusText ? ' | ' + pairStatusText : ''}\nАудиторія: <b>${item.room}</b> | 🕑 <b>${time}</b>` + "\n\n";
-      }
-
-
-      ctx.reply(scheduleResult);
-
-    } catch (err) {
-      await ctx.api.editMessageText(
-        ctx.chat!.id,
-        loadingMsg.message_id,
-        '❌ Не вдалося отримати дані'
-      );
-      console.log(err)
-    }
+    await getSchedule(ctx);
   })
 
   .text("Пара зараз", async (ctx) => {
